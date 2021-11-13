337c8c8a874a4d1a2971c3de0befcd055e12f636
Lee WonJoong <realjoong@gmail.com>
2021-11-13 16:32:03 +0900

    [feat] activity 초기화

--- app/src/main/java/com/sopt/jointseminargroupfour/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/jointseminargroupfour/ui/main/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.sopt.jointseminargroupfour.ui
+package com.sopt.jointseminargroupfour.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sopt.jointseminargroupfour.R
+import com.sopt.jointseminargroupfour.databinding.ActivityMainBinding
+import com.sopt.jointseminargroupfour.util.BaseView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseView.BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
