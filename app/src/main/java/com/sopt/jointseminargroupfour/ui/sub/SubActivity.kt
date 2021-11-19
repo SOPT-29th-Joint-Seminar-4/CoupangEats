21a96acc7018cbaa060015e83377c7fe5ace7f0d
Lee WonJoong <realjoong@gmail.com>
2021-11-19 09:28:33 +0900

    [feat] sub activity bottom navigation 추가

--- app/src/main/java/com/sopt/jointseminargroupfour/ui/sub/SubActivity.kt
+++ app/src/main/java/com/sopt/jointseminargroupfour/ui/sub/SubActivity.kt
@@ -12,19 +12,14 @@ class SubActivity: BaseView.BaseActivity<ActivitySubBinding>(R.layout.activity_s
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         initAdapter()
-
         initBackBtn()
-
         setContentView(binding.root)
     }
 
     private fun initAdapter() {
         recommendedMenuAdapter = RecommendedMenuAdapter()
-
         binding.rvRecommended.adapter = recommendedMenuAdapter
-
         recommendedMenuAdapter.recommendedMenuList.addAll(
             listOf(
                 RecommendedMenuItemData(
@@ -35,7 +30,6 @@ class SubActivity: BaseView.BaseActivity<ActivitySubBinding>(R.layout.activity_s
                     "18,000원",
                     "새 식용유를 매일 사용하여 건강하고 깨끗한 맛.\n 얇은 튀김옷으로 칼로리를 낮춪 담백한 치킨."
                 ),
-
                 RecommendedMenuItemData(
                     R.drawable.img_store_7,
                     "피자테리아",
@@ -44,7 +38,54 @@ class SubActivity: BaseView.BaseActivity<ActivitySubBinding>(R.layout.activity_s
                     "16,000원",
                     "얇은 도우 두겹사이에 고구마무스 가득~\n 세블락과 화이트 허브 소세지가 추가된 프리미..."
                 ),
-
+                RecommendedMenuItemData(
+                    R.drawable.img_store_8,
+                    "라비떼",
+                    "쿠치나",
+                    "152명이 만족한 메뉴",
+                    "18,000원",
+                    "꽃게 새우 오징어 갑오징어 바지락 홍합\n 토마토소스와 누룽지가 들어간 매콤한 파스타..."
+                ),
+                RecommendedMenuItemData(
+                    R.drawable.img_store_6,
+                    "둘둘치킨 서현점",
+                    "후라이드치킨",
+                    "46명이 만족한 메뉴",
+                    "18,000원",
+                    "새 식용유를 매일 사용하여 건강하고 깨끗한 맛.\n 얇은 튀김옷으로 칼로리를 낮춪 담백한 치킨."
+                ),
+                RecommendedMenuItemData(
+                    R.drawable.img_store_7,
+                    "피자테리아",
+                    "더블크러스트골드",
+                    "265명이 만족한 메뉴",
+                    "16,000원",
+                    "얇은 도우 두겹사이에 고구마무스 가득~\n 세블락과 화이트 허브 소세지가 추가된 프리미..."
+                ),
+                RecommendedMenuItemData(
+                    R.drawable.img_store_8,
+                    "라비떼",
+                    "쿠치나",
+                    "152명이 만족한 메뉴",
+                    "18,000원",
+                    "꽃게 새우 오징어 갑오징어 바지락 홍합\n 토마토소스와 누룽지가 들어간 매콤한 파스타..."
+                ),
+                RecommendedMenuItemData(
+                    R.drawable.img_store_6,
+                    "둘둘치킨 서현점",
+                    "후라이드치킨",
+                    "46명이 만족한 메뉴",
+                    "18,000원",
+                    "새 식용유를 매일 사용하여 건강하고 깨끗한 맛.\n 얇은 튀김옷으로 칼로리를 낮춪 담백한 치킨."
+                ),
+                RecommendedMenuItemData(
+                    R.drawable.img_store_7,
+                    "피자테리아",
+                    "더블크러스트골드",
+                    "265명이 만족한 메뉴",
+                    "16,000원",
+                    "얇은 도우 두겹사이에 고구마무스 가득~\n 세블락과 화이트 허브 소세지가 추가된 프리미..."
+                ),
                 RecommendedMenuItemData(
                     R.drawable.img_store_8,
                     "라비떼",
@@ -53,13 +94,9 @@ class SubActivity: BaseView.BaseActivity<ActivitySubBinding>(R.layout.activity_s
                     "18,000원",
                     "꽃게 새우 오징어 갑오징어 바지락 홍합\n 토마토소스와 누룽지가 들어간 매콤한 파스타..."
                 )
-
-
             )
         )
-
         binding.tvCount.text ="총 ${recommendedMenuAdapter.itemCount}개"
-
         recommendedMenuAdapter.notifyDataSetChanged()
     }
 
@@ -68,5 +105,4 @@ class SubActivity: BaseView.BaseActivity<ActivitySubBinding>(R.layout.activity_s
             finish()
         }
     }
-
 }
